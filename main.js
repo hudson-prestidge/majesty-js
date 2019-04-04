de0046c338433f4873948eb26c6178ce1a378db8
Hudson Burns Prestidge <hudson.prestidge@gmail.com>
2019-04-05 05:01:26 +1300

    unit moves down and right and can detect collision with goal

--- main.js
+++ main.js
@@ -1,4 +1,47 @@
 window.onload = function() {
   const unit = document.querySelector('.unit')
-  
+  const goal = document.querySelector('.goal')
+    unit.style.left = '1px'
+    unit.style.top = '1px'
+    unit.style.bottom = '1px'
+    unit.style.right = '1px'
+    setInterval(function() {
+      if(!collision(unit, goal)){
+        moveRight(unit)
+        moveDown(unit)
+      } else {
+        console.log('collision');
+      }
+    }, 3000)
+
+  }
+
+function moveUnit (unit) {
+
+}
+
+function moveRight (unit) {
+  let left = parseInt(unit.style.left.slice(0, unit.style.left.length-2))
+  left += 1;
+  unit.style.left = `${left}px`
+}
+
+function moveDown (unit) {
+  let top = parseInt(unit.style.top.slice(0, unit.style.top.length-2))
+  top += 1;
+  unit.style.top = `${top}px`
+}
+
+function collision(unit, goal){
+  unitX = unit.offsetLeft
+  unitY = unit.offsetTop
+  unitH = unit.offsetHeight
+  unitW = unit.offsetWidth
+
+  goalX = goal.offsetLeft
+  goalY = goal.offsetTop
+  goalH = goal.offsetHeight
+  goalW = goal.offsetWidth
+
+  return (unitY + unitH > goalY && unitY < goalY + goalH && unitX + unitW > goalX && unitX < goalX + goalW)
 }
--- readme.txt
+++ readme.txt
@@ -2,5 +2,6 @@ Simple exercise in pathfinding in js that could one day branch out into a game.
 
 To implement:
 - Movement
+- Collision
 - Moving to goal (shown in green)
 - Moving around obstacles
